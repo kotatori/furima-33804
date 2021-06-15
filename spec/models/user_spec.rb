dc4d56acc2df2f449ac55070bcd76320a1a60972
kotatori <kouta1313baske@ezweb.ne.jp>
2021-06-15 21:06:37 +0900

    テストコードの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -64,13 +64,13 @@ RSpec.describe User, type: :model do
       it 'passwordが全角のみだと登録できない' do
         @user.password = 'あああああああ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password", "Password Password Include both letters and numbers")
+        expect(@user.errors.full_messages).to include("Password Password Include both letters and numbers")
       end
 
       it 'passwordが半角数字のみだと登録できない' do
         @user.password = '111111'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+        expect(@user.errors.full_messages).to include("Password Include both letters and numbers")
       end
       it 'last_nameが空では登録できない' do
         @user.last_name = ''
