3cd9a3d49404c087069c7fa48fc0cd0d7921f2a7
kotatori <kouta1313baske@ezweb.ne.jp>
2021-06-20 23:51:31 +0900

    ビュー、コントローラの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new, :create]
-  before_action :set_item, only:[:show]
+  before_action :authenticate_user!, only:[:new, :create, :edit, :update]
+  before_action :set_item, only:[:show, :edit, :update]
+  before_action :move, only:[:edit, :update]
 
   def index
     @items = Item.all.order(created_at: "DESC")
@@ -22,11 +23,38 @@ class ItemsController < ApplicationController
       end
   end
 
+  def edit
+    # 事後学習のため残す
+    # if user_signed_in?
+    #   if current_user.id != @item.user_id
+    #     redirect_to action: :index
+    #   end
+    # else
+    #   redirect_to user_session_path
+    # end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
     def item_params
       params.require(:item).permit(:product_name, :text, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
     end
 
     def set_item
       @item = Item.find(params[:id])
+    end
+
+    def move
+      if current_user.id != @item.user_id
+        redirect_to action: :index
+      end
     end
 end
