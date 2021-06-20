4e31a7b996ad42c993a6992cdb75a3c0ceada9b9
kotatori <kouta1313baske@ezweb.ne.jp>
2021-06-20 21:10:02 +0900

    ビュー、コントローラの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only:[:new, :create]
+  before_action :set_item, only:[:show]
 
   def index
     @items = Item.all.order(created_at: "DESC")
@@ -25,4 +26,7 @@ class ItemsController < ApplicationController
       params.require(:item).permit(:product_name, :text, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
     end
 
+    def set_item
+      @item = Item.find(params[:id])
+    end
 end
